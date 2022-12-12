17dd12dab04c9f48e5ae6860bdbd1c77b8262333
DhioAWP <dhioanugerah0@gmail.com>
2022-12-12 16:38:20 +0700

    create public void insert

--- CircularQueues/Program.cs
+++ CircularQueues/Program.cs
@@ -12,8 +12,12 @@ namespace CircularQueues
         int[] queue_array = new int[5];
         public Program()
         {
-            /*initializing the values of the variables REAR and FRONT to -1 to indicate that
-             * the queue is intitally empty.*/
+            FRONT = -1;
+            REAR = -1;
+        }
+        public void insert(int element)
+        {
+            
         }
     }
 }
